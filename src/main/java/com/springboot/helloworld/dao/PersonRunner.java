fcedecdf5be3fd6dd839db87a6b1076058e4f33c
divine <mefistofell081992@gmail.com>
2018-03-06 19:42:12 +0200

    output persons into terminal

--- src/main/java/com/springboot/helloworld/dao/PersonRunner.java
+++ src/main/java/com/springboot/helloworld/dao/PersonRunner.java
@@ -1,5 +1,7 @@
 package com.springboot.helloworld.dao;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 
 public class PersonRunner implements CommandLineRunner{
@@ -11,12 +13,18 @@ public class PersonRunner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		/*Person p1 = new Person("Adam", 15);
+		Person p1 = new Person("Adam", 15);
 		Person p2 = new Person("Glen", 20);
 		Person p3 = new Person("Morris", 36);
 		personRepository.save(p1);
 		personRepository.save(p2);
-		personRepository.save(p3);*/
+		personRepository.save(p3);
+		getPersons();
+	}
+
+	private void getPersons() {
+		List<Person> persons = personRepository.findAll();	
+		persons.forEach(System.out::println);
 	}
 
 }
