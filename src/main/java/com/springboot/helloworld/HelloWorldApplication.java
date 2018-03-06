7d958e1da3aaf4f2ff758979922e8a003a3e1605
divine <mefistofell081992@gmail.com>
2018-03-06 19:56:38 +0200

    change CLR

--- src/main/java/com/springboot/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springboot/helloworld/HelloWorldApplication.java
@@ -1,13 +1,32 @@
 package com.springboot.helloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.springboot.helloworld.dao.Person;
+import com.springboot.helloworld.dao.PersonRepository;
+
 @SpringBootApplication
 @ComponentScan(basePackages ="com.springboot.helloworld")
-public class HelloWorldApplication {
-
+public class HelloWorldApplication implements CommandLineRunner{
+	private PersonRepository personRepository;
+	
+	public HelloWorldApplication (PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Person p1 = new Person("Adam", 15);
+		Person p2 = new Person("Glen", 20);
+		Person p3 = new Person("Morris", 36);
+		personRepository.save(p1);
+		personRepository.save(p2);
+		personRepository.save(p3);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
