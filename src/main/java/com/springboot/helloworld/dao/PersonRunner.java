4dd58242b57636c9a1bbf4859caa0699383606f8
divine <mefistofell081992@gmail.com>
2018-03-06 19:37:17 +0200

    detecting fails

--- src/main/java/com/springboot/helloworld/dao/PersonRunner.java
+++ src/main/java/com/springboot/helloworld/dao/PersonRunner.java
@@ -11,12 +11,12 @@ public class PersonRunner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		Person p1 = new Person("Adam", 15);
+		/*Person p1 = new Person("Adam", 15);
 		Person p2 = new Person("Glen", 20);
 		Person p3 = new Person("Morris", 36);
 		personRepository.save(p1);
 		personRepository.save(p2);
-		personRepository.save(p3);
+		personRepository.save(p3);*/
 	}
 
 }
