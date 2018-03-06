ca91383047c9ab1244bea04ae578fa23c736cced
divine <mefistofell081992@gmail.com>
2018-03-06 20:11:00 +0200

    setup database config on heroku machine

--- src/main/java/com/springboot/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springboot/helloworld/HelloWorldApplication.java
@@ -1,12 +1,8 @@
 package com.springboot.helloworld;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.springboot.helloworld.dao.Person;
-import com.springboot.helloworld.dao.PersonRepository;
 
 @SpringBootApplication
 @ComponentScan(basePackages ="com.springboot.helloworld")
